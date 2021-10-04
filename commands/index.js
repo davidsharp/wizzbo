3833da2c5f3734f152d50c9efbff5961ed73e8be
davidsharp <hello@davidsharp.codes>
2021-10-04 10:24:36 +0100

    magic 8-ball-like slash command [wip]

--- commands/index.js
+++ commands/index.js
@@ -3,9 +3,11 @@ const { Collection } = require('discord.js')
 const Ping = require('./ping')
 const ServerInfo = require('./serverInfo')
 const UserInfo = require('./userInfo')
+const Scry = require('./scry')
 
 const commands = [
   //Ping,
+  Scry,
   ServerInfo,
   UserInfo,
 ]
---
+++ commands/scry.js
@@ -0,0 +1,31 @@
+const { SlashCommandBuilder } = require('@discordjs/builders')
+
+const responses = [
+  `it is doubtful`,
+  `absolutely`,
+  `it is unclear to wizzbo`,
+  `ask me like you mean it`,
+  `look into yourself to find the answer`,
+  `perhaps another has the answer`,
+]
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('scry')
+    .setDescription('see the future 🔮')
+    .addStringOption(option => option.setName('question').setDescription('what should wizzbo seek?'))
+    //.addUserOption(option => option.setName('target').setDescription('Select a user'))
+    //.addChannelOption(option => option.setName('destination').setDescription('Select a channel'))
+    //.addRoleOption(option => option.setName('muted').setDescription('Select a role'))
+    .addMentionableOption(option => option.setName('fate').setDescription('discover the fate of whom?')),
+  execute: async interaction => {
+    const question = interaction.options.getString('question')
+    const fate = interaction.options.getString('fate')
+
+    console.log(question,fate)
+
+    if(question) await interaction.reply(`_"${question}"_ | wizzbo says ${responses[Math.floor(Math.random()*responses.length)]}`)
+    else if(fate) await interaction.reply(`wizzbo is unsure of their fate`);
+    else await interaction.reply({content:`wizzbo needs more info (add a question or a user)`,ephemeral:true});
+  },
+}
