521803c48e2807c31012cc9fd11fb0b634fa738b
davidsharp <hello@davidsharp.codes>
2021-10-04 16:40:32 +0100

    serve up static files

--- webserver.js
+++ webserver.js
@@ -3,6 +3,8 @@ const path = require('path')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './index.html'))
 })
