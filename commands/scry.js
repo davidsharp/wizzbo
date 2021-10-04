3927fa094264f092fac72814d7e4fb62bc389f94
davidsharp <hello@davidsharp.codes>
2021-10-04 11:46:12 +0100

    [wip] reply with an embed (missing permissions)

--- commands/scry.js
+++ commands/scry.js
@@ -1,3 +1,4 @@
+const { MessageEmbed } = require('discord.js')
 const { SlashCommandBuilder } = require('@discordjs/builders')
 const fraktur = require('fraktur');
 
@@ -20,22 +21,37 @@ const fatedAdjectives = [
   `bleak`,`promising`,`surprising`
 ]
 
+// inside a command, event listener, etc.
+const embed = ({asker,question,response}) => (
+  new MessageEmbed()
+    .setColor('#fedf00')
+    .setAuthor(asker)
+    .setDescription(question)
+    .addFields(
+      { name: '🔮 wizzbo says 🔮', value: response },
+    )
+    .setTimestamp()
+    //.setFooter('Some footer text here', 'https://i.imgur.com/AfFp7pu.png')
+)
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('scry')
     .setDescription('see the future 🔮')
     .addStringOption(option => option.setName('question').setDescription('what should wizzbo seek?'))
     .addMentionableOption(option => option.setName('fate').setDescription('discover the fate of whom?')),
-  execute: async interaction => {
+  execute: async (interaction,{client}) => {
     const question = interaction.options.getString('question')
     const fate = interaction.options.getMentionable('fate')
 
-    if(fate)console.log(fate)
+    const channel = await client.channels.fetch(interaction.channelId)
+
+    console.log(channel)
 
     let fated
     if(fate) fated = fate.name || fate.nickname || (fate.user && fate.user.username) || 'fated'
 
-    if(question) await interaction.reply(`_"${question}"_ 🔮 wizzbo says "${fraktur(responses[Math.floor(Math.random()*responses.length)])}""`)
+    if(question) channel.send({ embeds: [embed({asker:'anonymous',question,response:fraktur(responses[Math.floor(Math.random()*responses.length)])})] });
+    //await interaction.reply(`_"${question}"_ 🔮 wizzbo says "${fraktur(responses[Math.floor(Math.random()*responses.length)])}""`)
     else if(fate) await interaction.reply(`${fraktur(fates[Math.floor(Math.random()*fates.length)](fated))}`);
     else await interaction.reply({content:`wizzbo needs more info (add a question or a user)`,ephemeral:true});
   },
