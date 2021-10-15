2527e8182e7c1feeb0336436045be3cee12494b3
davidsharp <hello@davidsharp.codes>
2021-10-15 09:37:08 +0100

    Makes voting parameters non-optional

--- commands/vote.js
+++ commands/vote.js
@@ -4,8 +4,8 @@ const setupvote = {
   data: new SlashCommandBuilder()
     .setName('setupvote')
     .setDescription('sets up an anonymous poll `/vote` to participate')
-    .addIntegerOption(option => option.setName('players').setDescription('how many people are voting?'))
-    .addIntegerOption(option => option.setName('stories').setDescription('how many items to vote from?'))
+    .addIntegerOption(option => option.setName('players').setDescription('how many people are voting?').setRequired(true))
+    .addIntegerOption(option => option.setName('stories').setDescription('how many items to vote from?').setRequired(true))
     .addIntegerOption(option => option.setName('votes').setDescription('[optional, default:2] how many votes per person?')),
   execute: async interaction => {
     const players = interaction.options.getInteger('players')
@@ -31,7 +31,7 @@ const vote = {
   data: new SlashCommandBuilder()
     .setName('vote')
     .setDescription('anonymously vote in the current poll')
-    .addStringOption(option => option.setName('vote').setDescription('vote in the current poll')),
+    .addIntegerOption(option => option.setName('vote').setDescription('vote in the current poll').setRequired(true)),
   execute: async interaction => {
     await interaction.reply({content:`wizzbo needs more info (add a question or a user)`,ephemeral:true});
   },
