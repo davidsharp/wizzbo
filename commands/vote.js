902a10a0776f80c00ac94b809697cb45e5240f57
davidsharp <hello@davidsharp.codes>
2021-10-15 22:55:29 +0100

    WIP follow up on votes completed

--- commands/vote.js
+++ commands/vote.js
@@ -9,12 +9,12 @@ const setupvote = {
     .setDescription('sets up an anonymous poll `/vote` to participate')
     .addIntegerOption(option => option.setName('players').setDescription('how many people are voting?').setRequired(true))
     .addIntegerOption(option => option.setName('entries').setDescription('how many items to vote from?').setRequired(true))
-    .addIntegerOption(option => option.setName('votes').setDescription('[optional, default:2] how many votes per person?'))
+    .addIntegerOption(option => option.setName('votes').setDescription('[optional, default:3] how many votes per person?'))
     .addBooleanOption(option => option.setName('weighted').setDescription('[optional, default:false] are votes weighted (or worth equal)?')),
   execute: async interaction => {
     const players = interaction.options.getInteger('players')
     const entries = interaction.options.getInteger('entries')
-    const votes = (interaction.options.getInteger('votes') || 2)
+    const votes = (interaction.options.getInteger('votes') || 3)
     const weighted = !!interaction.options.getBoolean('weighted')
     const { guildId } = interaction
 
@@ -22,7 +22,8 @@ const setupvote = {
 
     console.log(poll)
 
-    await interaction.reply({content:`[Input test:] players (${players}), entries (${entries}), votes (${votes}), weighted (${weighted})`,ephemeral:true});
+    //await interaction.reply({content:`[Input test:] players (${players}), entries (${entries}), votes (${votes}), weighted (${weighted})`,ephemeral:true});
+    await interaction.reply({content:`A poll has been created, please vote from 1-${entries}, you have ${votes} vote(s)${weighted && votes>1?', please vote starting with your highest':''}`});
   }
 }
 
@@ -40,7 +41,9 @@ const vote = {
   data: new SlashCommandBuilder()
     .setName('vote')
     .setDescription('anonymously vote in the current poll')
-    .addIntegerOption(option => option.setName('vote').setDescription('vote in the current poll').setRequired(true)),
+    .addIntegerOption(option => option.setName('vote').setDescription('vote in the current poll').setRequired(true))
+    //.addIntegerOption(option => option.setName('place').setDescription('position to be voted for'))
+    ,
   execute: async interaction => {
     const vote = interaction.options.getInteger('vote')
 
@@ -52,10 +55,13 @@ const vote = {
 
     poll.voters[interaction.user].push(vote)
 
+    if(!poll.votesCast) poll.votesCast = 0
+    poll.votesCast++
+
     console.log(poll)
 
-    // TODO - say how many votes are left
     await interaction.reply({content:`Thanks, you voted for ${vote}, you have ${poll.votes-poll.voters[interaction.user].length} votes left`,ephemeral:true})
+    if(poll.votesCast == poll.players * poll.votes) await interaction.followUp({content:`Poll complete!`,ephemeral:false})
   }
 }
 
